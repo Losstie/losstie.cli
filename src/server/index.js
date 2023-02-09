721522caf2ee5e1e3241eaa71c640baf9b941a3f
losstie <losstie@outlook.com>
2023-02-09 22:40:38 +0800

    feat: server初步

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,18 @@
 const koa = require('koa');
+const webpack = require('webpack');
+const bodyParser = require('body-parser');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 
 module.exports = function run(webpackConfig, serverConfig){
+    let localServer = koa();
+    let proxyServer = koa();
+
+    localServer.use(bodyParser.json());
+    localServer.use(bodyParser.urlencoded({extended: false}));
+
+    // ----------------本地mock服务-------------
+
+
 
 }
